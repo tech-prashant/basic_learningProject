1fb80a00670fdd734f56f3963fb805ba39c954ce
prashant sharma <prashant6515@gmail.com>
2021-03-23 00:06:55 +0530

    basic project structure preparation

---
+++ src/main/java/com/basic/tech/basiclearning/BasiclearningApplication.java
@@ -0,0 +1,13 @@
+package com.basic.tech.basiclearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasiclearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasiclearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basic/tech/basiclearning/BasiclearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.tech.basiclearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasiclearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
